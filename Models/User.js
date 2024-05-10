15c5209393efd9a40eb5065280d7292f76278fd2
THARSIKAN <tharsikan@localhost.localdomain>
2024-05-10 16:59:32 +0530

    update

--- Models/User.js
+++ Models/User.js
@@ -3,11 +3,32 @@ const { Schema } = require("mongoose");
 const mongoose = require("../Mongodb/connectDB");
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
-  age: Number,
-  country: String,
+  name: {
+    type: String,
+    required: true, // Name is required
+    minlength: 2, // Minimum length of name
+    maxlength: 50, // Maximum length of name
+  },
+  email: {
+    type: String,
+    required: true, // Email is required
+    unique: true, // Email must be unique
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, // Email format validation
+  },
+  phone: {
+    type: String,
+    required: true, // Phone is required // Phone number format validation
+  },
+  age: {
+    type: Number,
+    required: true, // Age is required
+    min: 18, // Minimum age
+    max: 120, // Maximum age
+  },
+  country: {
+    type: String,
+    required: true, // Country is required
+  },
 });
 
 const User = mongoose.model("User", userSchema);
--- Routes/add.js
+++ Routes/add.js
@@ -7,6 +7,15 @@ const User = require(".././Models/User");
 router.post("/api/add", async (req, res) => {
   try {
     const data = req.body;
+
+    const email = data.email;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).send("User with this email already exists");
+    }
+
     await User.create(data);
     res.status(200).send("Data inserted successfully");
   } catch (err) {
